026332f89fcf83a339ed603de66a0860f6e7108b
Connor Clark <connordeclark@gmail.com>
2023-04-11 12:14:44 -0500

    Building start up and working on Migrations

--- Models/ApplicationContext.cs
+++ Models/ApplicationContext.cs
@@ -5,7 +5,6 @@ namespace jobHunterBackEnd.Models
     public class ApplicationContext : DbContext
     {
         public ApplicationContext(DbContextOptions<ApplicationContext> options) : base(options) {}
-        // public DbSet<MyClass> MyClassTable { get; set; }
 
         public DbSet<Application> Applications { get; set; }
         public DbSet<Company> Companies { get; set; }
